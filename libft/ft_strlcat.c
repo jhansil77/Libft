f5388073106f57d791f32a984180be25b4381d36
jhannsil <jhansilva369@gmail.com>
2026-05-29 13:53:34 +0200

    faltan 9 funcinones

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -16,10 +16,10 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 	size_t i;
 
 	i = 0;
-	dest_len = ft_str_len(dest);
 	src_len = ft_str_len(src);
-	if (n == 0)
-		return (dest_len + src_len);
+	dest_len = 0;
+	while (dest[dest_len] && dest_len < n)
+		dest_len++;
 	if (n <= dest_len)
 		return (n + src_len);
 	while (src[i] != '\0' && dest_len + i < n - 1)
