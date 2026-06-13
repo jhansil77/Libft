69d9fc8e2e74ea82fbca91f6ff245cbfe688eb72
jhannsil <jhansilva369@gmail.com>
2026-06-13 15:25:59 +0200

    Arreglado

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -19,11 +19,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	s3_len = ft_strlen(s1) + ft_strlen((s2));
-	s3 = (char *)ft_calloc(s3_len + 1, sizeof(char));
+	s3_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	s3 = (char *)ft_calloc(s3_len, sizeof(char));
 	if (!s3)
 		return (NULL);
-	ft_strlcpy(s3, s1, ft_strlen(s1) + 1);
-	ft_strlcat(s3, s2, s3_len + 1);
+	ft_strlcpy(s3, s1, s3_len);
+	ft_strlcat(s3, s2, s3_len);
 	return (s3);
 }
