e810eaa33197c767fe29e79d7648c1e0a0aed134
jhannsil77 <jhansilva369@gmail.com>
2026-06-16 14:21:30 +0200

    arreglado

--- libft.h
+++ libft.h
@@ -13,13 +13,9 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <string.h>
-# include <stddef.h>
-# include <stdlib.h>
-# include <limits.h>
-# include <stdint.h>
-# include <stdio.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
 
 typedef struct s_list
 {
